e5206e2c1e41dd8ee72a59aaea35257dccf70de6
AIMAR <abengolano2@gmail.com>
2025-02-25 12:19:36 +0100

    nuevo zoologivo v2

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,70 @@
+<x-app-layout>
+    @vite(['resources/css/dashboard.css', 'resources/js/dashboard.js'])
+  
+
+
+   
+    
+        <div class="bg-cover bg-center h-screen w-full" style="background-image: url('{{ asset('imagenes/fondo.jpg') }}');">
+            <!-- Contenido adicional aquí -->
+            <div class="p-6 text-white">
+                
+               
+            </div>
+           
+        </div>
+        <div>
+            <h1 class="text-2xl font-bold">Título</h1>
+            <p class="mt-4">Descripción o contenido adicional.</p>
+            <ul>
+                <li>
+                  <a href="#" contenteditable>
+                    <h2>Title #1</h2>
+                    <p>Text Content #1</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" contenteditable>
+                    <h2>Title #2</h2>
+                    <p>Text Content #2</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" contenteditable>
+                    <h2>Title #3</h2>
+                    <p>Text Content #3</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" contenteditable>
+                    <h2>Title #4</h2>
+                    <p>Text Content #4</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" contenteditable>
+                    <h2>Title #5</h2>
+                    <p>Text Content #5</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" contenteditable>
+                    <h2>Title #6</h2>
+                    <p>Text Content #6</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" contenteditable>
+                    <h2>Title #7</h2>
+                    <p>Text Content #7</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" contenteditable>
+                    <h2>Title #8</h2>
+                    <p>Text Content #8</p>
+                  </a>
+                </li>
+              </ul>
+        </div>
+</x-app-layout>
